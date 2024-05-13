b6c6726ef893eba291836ba650bd495cb865bdd9
Yisehak Abreham <yisehak.aw@gmail.com>
2024-05-13 08:16:48 +0300

    add tabs component

---
+++ app/components/tabs.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "@remix-run/react";
+
+interface TabItem {
+  label: string;
+  href: string;
+}
+
+const Tabs = ({ tabs }: { tabs: TabItem[] }) => {
+  return (
+    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+      <ul className="flex flex-wrap -mb-px px-12">
+        {tabs.map((t) => (
+          <li className="me-2">
+            <NavLink
+              to={t.href}
+              className={({ isActive }) =>
+                `inline-block p-2 border-b-2 ${
+                  isActive
+                    ? "font-bold text-black  border-black active"
+                    : "border-transparent"
+                }`
+              }
+            >
+              {t.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Tabs;
