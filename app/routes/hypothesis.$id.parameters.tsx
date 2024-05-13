8d37a17b5fe9ab7196dc1c4b128c763865ce1998
Yisehak Abreham <yisehak.aw@gmail.com>
2024-05-13 09:06:08 +0300

    implement the hypothesis parameters page

--- app/routes/hypothesis.$id.parameters.tsx
+++ app/routes/hypothesis.$id.parameters.tsx
@@ -1,3 +1,28 @@
+import { Download, FileText } from "lucide-react";
+
 export default () => {
-  return <h1>Parameters</h1>;
+  return (
+    <div className="mt-4 pt-4 px-12">
+      <dl>
+        <dt className="font-bold">SNV or indel</dt>
+        <dd className="mb-4">{"c.56A>T"}</dd>
+
+        <dt className="font-bold">Phenotype / Trait</dt>
+        <dd className="mb-4">Obesity</dd>
+
+        <dt className="font-bold">Tissue / Cell type</dt>
+        <dd className="mb-4">Cardiomyocytes</dd>
+
+        <dt className="font-bold mb-2">Co-expression network / data </dt>
+        <dd className="mb-4">
+          <div className="p-4 rounded border w-fit">
+            <FileText className="inline me-2" /> Co-expression data
+            <a href="#" className="ml-16">
+              <Download className="inline" />
+            </a>
+          </div>
+        </dd>
+      </dl>
+    </div>
+  );
 };
